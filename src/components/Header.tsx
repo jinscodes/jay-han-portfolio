e5f4a4b095daac135fa9a22f1dfa837b030a927a
{jinsung} <jayhan0215@gmail.com>
2024-12-19 19:51:54 +0900

    Menu styling
     - When clicking the menu, menu will be converted to bullet with animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,23 +10,61 @@ const Header = () => {
       <p className={st.name}>Jay Han</p>
       <p className={st.title}>Frontend & App Developer</p>
       <nav>
-        <NavLink
-          to="/"
-          className={menu === "" ? st.active : ""}
-          onClick={() => setMenu("")}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/projects"
-          className={menu === "projects" ? st.active : ""}
-          onClick={() => setMenu("projects")}
-        >
-          Projects
-        </NavLink>
-        <NavLink to="/info">Info</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-        <NavLink to="/faq">FAQ</NavLink>
+        {menu === "" ? (
+          <span className={st.bullet}>●</span>
+        ) : (
+          <NavLink
+            to="/"
+            className={menu === "" ? st.active : ""}
+            onClick={() => setMenu("")}
+          >
+            Home
+          </NavLink>
+        )}
+        {menu === "projects" ? (
+          <span className={st.bullet}>●</span>
+        ) : (
+          <NavLink
+            to="/projects"
+            className={menu === "projects" ? st.active : ""}
+            onClick={() => setMenu("projects")}
+          >
+            Projects
+          </NavLink>
+        )}
+        {menu === "info" ? (
+          <span className={st.bullet}>●</span>
+        ) : (
+          <NavLink
+            to="/info"
+            className={menu === "info" ? st.active : ""}
+            onClick={() => setMenu("info")}
+          >
+            Info
+          </NavLink>
+        )}
+        {menu === "contact" ? (
+          <span className={st.bullet}>●</span>
+        ) : (
+          <NavLink
+            to="/contact"
+            className={menu === "contact" ? st.active : ""}
+            onClick={() => setMenu("contact")}
+          >
+            Contact
+          </NavLink>
+        )}
+        {menu === "FAQ" ? (
+          <span className={st.bullet}>●</span>
+        ) : (
+          <NavLink
+            to="/FAQ"
+            className={menu === "FAQ" ? st.active : ""}
+            onClick={() => setMenu("FAQ")}
+          >
+            FAQ
+          </NavLink>
+        )}
       </nav>
     </section>
   );
