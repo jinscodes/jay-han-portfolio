f39f6a8015cda526407a83b54d2b8b8db64f9f15
{jinsung} <jayhan0215@gmail.com>
2024-12-23 20:26:32 +0900

    Publishing Info component

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,5 +1,45 @@
+import Arrow from "@assets/svg/arrow.svg?react";
+import st from "./info.module.scss";
+
 const Info = () => {
-  return <div>Info</div>;
+  return (
+    <section className={st.info}>
+      <ul>
+        <li>
+          Google
+          <Arrow />
+        </li>
+        <li>
+          Instagram
+          <Arrow />
+        </li>
+        <li>
+          LinkedIn
+          <Arrow />
+        </li>
+        <br />
+        <br />
+        <li>
+          Jay's Blog
+          <Arrow />
+        </li>
+        <li>
+          Github
+          <Arrow />
+        </li>
+        <br />
+        <br />
+        <li>
+          Stonybrook University at SUNY
+          <Arrow />
+        </li>
+        <li>
+          Union Contents
+          <Arrow />
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Info;
