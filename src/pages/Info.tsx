d109e958d51fb20fc03c16d9ff247bd6a9bb6757
{jinsung} <jayhan0215@gmail.com>
2024-12-24 16:45:32 +0900

    Refactoring Info component

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,63 +1,77 @@
 import Arrow from "@assets/svg/arrow.svg?react";
 import st from "./info.module.scss";
 
-interface LinksType {
+interface LinkGroup {
+  title?: string;
+  links: Links[];
+}
+
+interface Links {
   domain: string;
   url: string;
 }
 
 const Info = () => {
-  const techLinks: LinksType[] = [
+  const linkGroup: LinkGroup[] = [
     {
-      domain: "LinkedIn",
-      url: "https://www.linkedin.com/in/jin-sung-han-49974920a/",
+      title: "Tech",
+      links: [
+        {
+          domain: "LinkedIn",
+          url: "https://www.linkedin.com/in/jin-sung-han-49974920a/",
+        },
+        {
+          domain: "Jay's Blog",
+          url: "https://jay-h-blog.vercel.app/",
+        },
+        {
+          domain: "Github",
+          url: "https://github.com/jinscodes",
+        },
+      ],
     },
     {
-      domain: "Jay's Blog",
-      url: "https://jay-h-blog.vercel.app/",
-    },
-    {
-      domain: "Github",
-      url: "https://github.com/jinscodes",
+      title: "Personal",
+      links: [
+        {
+          domain: "Facebook",
+          url: "https://www.facebook.com/profile.php?id=100021292740024",
+        },
+        {
+          domain: "Instagram",
+          url: "https://www.instagram.com/jin_sung_h/",
+        },
+      ],
     },
   ];
-  const personalLinks: LinksType[] = [
-    {
-      domain: "Facebook",
-      url: "https://www.facebook.com/profile.php?id=100021292740024",
-    },
-    {
-      domain: "Instagram",
-      url: "https://www.instagram.com/jin_sung_h/",
-    },
+
+  const otherItems = [
+    "Stonybrook University at SUNY",
+    "Union Contents",
+    "Team Breeders",
   ];
 
   return (
     <section className={st.info}>
       <ul>
-        {techLinks.map((el) => (
-          <li>
-            <a href={el.url} target="_blank">
-              {el.domain}
-              <Arrow />
-            </a>
-          </li>
+        {linkGroup.map((group) => (
+          <>
+            {group.links.map((el) => (
+              <li>
+                <a href={el.url} target="_blank">
+                  {el.domain}
+                  <Arrow />
+                </a>
+              </li>
+            ))}
+            <br />
+            <br />
+          </>
         ))}
-        <br />
-        <br />
-        {personalLinks.map((el) => (
-          <li>
-            <a href={el.url} target="_blank">
-              {el.domain}
-              <Arrow />
-            </a>
-          </li>
+
+        {otherItems.map((el) => (
+          <li>{el}</li>
         ))}
-        <br />
-        <br />
-        <li>Stonybrook University at SUNY</li>
-        <li>Union Contents</li>
-        <li>Team Breeders</li>
       </ul>
     </section>
   );
