47f22c98db91d4839a6acf552f64e82292c00c12
{jinsung} <jayhan0215@gmail.com>
2025-01-02 18:32:14 +0900

    Connect to notion's portfolio

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,10 +2,13 @@ import Arrow from "@assets/svg/arrow.svg?react";
 import st from "./contact.module.scss";
 
 const Contact = () => {
+  const NOTION_PORTFOLIO_PAGE =
+    "https://www.notion.so/Developer-Portfolio-FE-Developer-168337c3bbd1804d9482c2316bb2a7d8";
+
   return (
     <section className={st.contact}>
       <div>
-        <a href="">
+        <a href={NOTION_PORTFOLIO_PAGE} target="_blank">
           To contact me
           <Arrow />
         </a>
